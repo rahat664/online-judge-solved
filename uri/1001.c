e1bae383f17b314f83039eb03e55ea3ec795c53c
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2019-09-16 11:43:36 +0600

    Update 1001.c

--- uri/1001.c
+++ uri/1001.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
    int a,b;
-   scanf("%d %d",&a,&b);
-   printf("X = %d\n",a+b);
+   scanf("%d%d", &a, &b);
+   printf("X = %d\n", a+b);
     return 0;
 }
