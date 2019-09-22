940625d7f7eb253f75151e083f82ec56ccf36a51
rahat854 <rahatkabir20@gmail.com>
2019-09-23 03:19:19 +0600

    origin master

---
+++ uri/1114.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+
+    while(1)
+    {
+        scanf("%d",&a);
+
+        if(a == 2002)
+        {
+            printf("Acesso Permitido\n");
+            break;
+        }
+
+        else
+        {
+            printf("Senha Invalida\n");
+        }
+
+    }
+
+    return 0;
+}
