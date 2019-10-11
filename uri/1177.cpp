bb0be0ec346ae4790ede6ef8d732dec9b339d1de
rahat854 <rahatkabir20@gmail.com>
2019-10-11 22:36:13 +0600

    1177 cpp

---
+++ uri/1177.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, a;
+
+    int N[1000];
+
+    cin >> t;
+     a = 0;
+    for(int i = 0; i < 1000; i++)
+        {
+            cout << "N[" << i << "] = " << a << endl;
+            a++;
+            if(a == t) a = 0;
+    }
+    return 0;
+}
