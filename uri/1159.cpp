9d0476a1dececa8d83a75483e1ceea6cb9a037f0
rahat854 <rahatkabir20@gmail.com>
2019-10-11 19:01:03 +0600

    1159 cpp

---
+++ uri/1159.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, soma;
+
+    while(true)
+    {
+        cin >> x;
+        if(x == 0)
+            break;
+
+        soma = 0;
+
+        if(x%2 == 0)
+            soma += x;
+        else
+            soma += ++x;
+
+        for(int i = 0; i < 4; i++)
+        {
+            x += 2;
+            soma += x;
+        }
+        cout << soma << "\n";
+    }
+    return 0;
+}
