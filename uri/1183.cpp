efd4c3e7156f5da8c9f9d04a5f7b130319eb4a68
rahat854 <rahat854>
2019-11-15 16:41:29 +0600

    1183 in cpp

---
+++ uri/1183.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    double a=0.0, M[12][12];
+    char T[2];
+    int C,x,y,z,p=1;
+    cin >> T;
+    for(x=0;x<=11;x++)
+    {
+        for(y=0; y<=11; y++)
+            cin >> M[x][y];
+    }
+    for(z=0; z<=11;z++)
+    {
+        for(C=p; C<=11;C++)
+            a+=M[z][C];
+        p++;
+    }
+    if(T[0]=='S')
+        printf("%.1lf\n",a);
+    else if(T[0]=='M')
+    {
+        a=a/66.0;
+        printf("%.1lf\n",a);
+    }
+    return 0;
+}
+
