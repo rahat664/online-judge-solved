8c212e94b49229423beabf5262800f8ddb3e0871
rahat854 <rahatkabir20@gmail.com>
2019-11-29 00:49:23 +0600

    weekly contest

---
+++ weekly contest/d.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin >> a >> b;
+
+    if(a<=200 && b <=200)
+    {
+        cout << a+b << endl;
+    }
+    return 0;
+}
