179114a6afb6a68b91ae4d7054372301062a22c0
rahat854 <rahatkabir20@gmail.com>
2019-11-29 00:47:21 +0600

    weekly contest

---
+++ weekly contest/e.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int t,i,n;
+        for(i=0;i<t;i++)
+        {
+            int n;
+
+            cin >> n;
+
+
+        }
+cout << "LCM(n) = " << boost::math::lcm(n) << endl;
+
+    }
+}
