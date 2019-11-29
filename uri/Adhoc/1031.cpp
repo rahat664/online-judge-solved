9751e7a6aaa8ea9427eea7a92d76e46db5b8fecf
rahat854 <rahatkabir20@gmail.com>
2019-11-29 23:16:58 +0600

    1031 adhoc

---
+++ uri/Adhoc/1031.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int remaining(int n, int k)
+{
+    int r = 0;
+    for (int i = 1; i < n; i++)
+        r = (r + k) % i;
+
+    return r;
+}
+
+int main(){
+    int n, x, y, j, num, pulo;
+    while(1)
+        {
+           cin >> n;
+           if(n == 0)
+           {
+               break;
+           }
+           y = 1;
+           for(;;)
+            {
+                   if(remaining(n,y) != 11)
+                   {
+                       y++;
+                   }
+                   else
+                   {
+                       break;
+                   }
+
+           }
+           cout << y << endl;
+    }
+    return 0;
+}
