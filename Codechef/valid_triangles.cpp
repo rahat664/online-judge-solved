97d92e8072d59c07bd51c5906a322ba4b8f0146f
rahat854 <rahatkabir20@gmail.com>
2020-11-05 14:50:49 +0600

    new

---
+++ Codechef/valid_triangles.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+	int test;
+	cin>>test;
+	while(test--)
+    {
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a+b+c==180)
+        {
+            cout<<"YES"<<endl;
+        }
+	        
+	    else
+        {
+            cout<<"NO"<<endl;
+        }
+	        
+	}
+	return 0;
+}
